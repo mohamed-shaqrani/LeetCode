29130bd924d4cd88eca3d20cfa03c6ae2f0a004d
Mohamed Shokrani <104221047+mohamed-shokrani@users.noreply.github.com>
2024-05-27 10:11:27 +0300

    SearchInsert

---
+++ LeetCode/Easy/SearchInsertV1.cs
@@ -0,0 +1,46 @@
+﻿
+namespace LeetCode.Easy;
+
+internal static class SearchInsert
+{
+    public static int SearchInsertV1(int[] nums, int target)
+    {
+        int before = 0;
+        int after = 0; ;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == target)
+            {
+                return target;
+            }
+            if (nums.Length - 1 == i && after == 0)
+            {
+                if (nums[i] > target)
+                {
+                    return i;
+                }
+                return i + 1;
+            }
+            if (nums[i] < target)
+            {
+                before = nums[i];
+                continue;
+            }
+            if (nums[i] > target)
+            {
+                after = nums[i];
+            }
+
+            if (after != 0 && before != 0)
+            {
+                return i;
+            }
+
+
+
+        }
+        return 0;
+    }
+}
+}
